def41d7d51956d73d20d242f0a844031f3babd3a
Jonatas <jonatas.iw@gmail.com>
2026-01-20 17:45:22 -0300

    FRONT-13 create vault client direct from frontend and then calling the back to register

--- src/infra/api-lp/vault.ts
+++ src/infra/api-lp/vault.ts
@@ -1,5 +1,5 @@
 import { apiLpPost } from "@/infra/api-lp/client";
-import type { CreateClientVaultResponse, CreateClientVaultRequest } from "@/domain/vault/types";
+import type { CreateClientVaultResponse, CreateClientVaultRequest, RegisterClientVaultResponse } from "@/domain/vault/types";
 
 export async function createClientVaultApi(params: {
   payload: CreateClientVaultRequest;
@@ -9,5 +9,16 @@ export async function createClientVaultApi(params: {
     "/vaults/create-client-vault",
     params.payload,
     params.accessToken,
+  );
+}
+
+export async function registerClientVaultApi(params: {
+  payload: any;
+  accessToken: string;
+}): Promise<RegisterClientVaultResponse> {
+  return apiLpPost(
+    "/vaults/register-client-vault",
+    params.payload,
+    params.accessToken
   );
 }
