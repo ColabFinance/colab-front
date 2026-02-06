0e6a3559eb1ef66701b524caa2290ca79f164578
Jonatas <jonatas.iw@gmail.com>
2026-02-06 19:06:13 -0300

    FRONT-45 implement swap reward

--- src/infra/evm/abis/clientVault.abi.ts
+++ src/infra/evm/abis/clientVault.abi.ts
@@ -204,6 +204,21 @@ export const ClientVaultAbi = [
     outputs: [],
   },
 
+  {
+    type: "function",
+    name: "swapExactInPancake",
+    stateMutability: "nonpayable",
+    inputs: [
+      { name: "tokenIn", type: "address" },
+      { name: "tokenOut", type: "address" },
+      { name: "fee", type: "uint24" },
+      { name: "amountIn", type: "uint256" },
+      { name: "amountOutMinimum", type: "uint256" },
+      { name: "sqrtPriceLimitX96", type: "uint160" },
+    ],
+    outputs: [{ name: "amountOut", type: "uint256" }],
+  },
+
   // daily harvest / compound / reward swap setters
   {
     type: "function",
